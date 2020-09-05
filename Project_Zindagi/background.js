68cf7e7d132a6cbb10ae5ee893d783b125f2269f
reneelinmitchell <reneelinmitchell@gmail.com>
2020-09-05 19:47:15 -0400

    no errorsgit add --allgit add --allgit add --all!

--- Project_Zindagi/background.js
+++ Project_Zindagi/background.js
@@ -8,9 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	// debugger;
-  if (changeInfo.url.includes("Mychart/inside.asp")) {
-    chrome.tabs.executeScript({file: "slides/home.js"});
-  } else if (changeInfo.url.includes("Mychart/Messaging/Review?mailbox")) {
-  	chrome.tabs.executeScript({file: "slides/messages.js"});
-  };
+	if ((changeInfo.url != null) && (changeInfo.url != "undefined")) {
+		if (changeInfo.url.includes("Mychart/inside.asp")) {
+		    chrome.tabs.executeScript({file: "slides/home.js"});
+		  } else if (changeInfo.url.includes("Mychart/Messaging/Review?mailbox")) {
+		  	chrome.tabs.executeScript({file: "slides/messages.js"});
+		  };
+	}
+  
 });
