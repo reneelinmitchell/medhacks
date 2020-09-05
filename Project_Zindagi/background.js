65f2321c660b27c24c0c3b6fd2f595a27ffac2d7
reneelinmitchell <reneelinmitchell@gmail.com>
2020-09-05 15:36:53 -0400

    updated zindagi

--- Project_Zindagi/background.js
+++ Project_Zindagi/background.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({file: "slides/slide1.js"});
+    chrome.tabs.executeScript({file: "slides/login.js"});
   });
 
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.url) {
-    chrome.tabs.executeScript({file: "slides/slide2.js"});
+    chrome.tabs.executeScript({file: "slides/home.js"});
   };
 });
