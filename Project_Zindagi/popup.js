6631433e53b21a0c6516d6ced8e835971d898b33
reneelinmitchell <reneelinmitchell@gmail.com>
2020-09-05 09:28:19 -0400

    with examples

--- Project_Zindagi/popup.js
+++ Project_Zindagi/popup.js
@@ -1,19 +1,23 @@
 console.log("hello there what's up");
 
-document.addEventListener('DOMContentLoaded', function() {
-  var headerText = document.getElementsByTagName('h1')[0].innerText;
-  var homePagePopup = document.getElementById('homePage');
-  var isHomePage = false;
-  $homePagePopup.toggle(shouldElementBeVisible == false);
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
 
-  if (headerText == "MyChart - Your secure online health connection") {
-    isHomePage = true;
-    console.log("isHomePage: " + isHomePage);
-    $homePagePopup.toggle(shouldElementBeVisible == true);
-  }
+  // var headerText = document.getElementsByTagName('h1')[0].innerText;
+  // var homePagePopup = document.getElementById('homePage');
+  // var isHomePage = false;
+  // $homePagePopup.toggle(shouldElementBeVisible == false);
 
-    // make sure that the user has done what is needed to advance 
-  }, false);
+  // if (headerText == "MyChart - Your secure online health connection") {
+  //   isHomePage = true;
+  //   console.log("isHomePage: " + isHomePage);
+  //   $homePagePopup.toggle(shouldElementBeVisible == true);
+  // }
+
+});
 
 // document.addEventListener('DOMContentLoaded', function() {
 //   var checkPageButton = document.getElementById('checkPage');
